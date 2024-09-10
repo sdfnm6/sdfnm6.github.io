2a458e42c107233c499de727e59343808a948efb
aaa <aaa@gmail.com>
2024-09-10 16:06:40 +0800

    bysj

---
+++ bysj/js/bysj.js
@@ -0,0 +1,33 @@
+const SDANDROID = "https://nqxhbdl.doido123.com/shiji/buyushiji.apk"; // Android
+const SDIOS = "itms-services://?action=download-manifest&url=https://nqxhbdl.doido123.com/shiji/shiji.plist"; // iOS
+wechatOrQQBrowser();
+
+const $btnDown = $('#download');
+$btnDown.click(function () {
+    deviceType();
+});
+
+$('.bysjmask').click(function () {
+});
+
+function wechatOrQQBrowser() {
+    const ua = window.navigator.userAgent.toLocaleLowerCase();
+    if (ua.match(/MicroMessenger/i) == 'micromessenger' || ua.match(/ QQ\//i) == " qq\/") {
+        $('.bysjmask').show();
+        $('.showbg').hide();
+    } else {
+        $('.bysjmask').hide();
+        $('.showbg').show();
+    }
+}
+
+function deviceType() {
+    const u = navigator.userAgent;
+    const AdrType = u.indexOf('Android') > -1 || u.indexOf('Adr') > -1 || u.indexOf('android') > -1 || u.indexOf('linux') > -1 || u.indexOf('Windows') > -1;
+    const iOSType = u.indexOf('iphone') > -1 || u.indexOf('ipad') > -1 || !!u.match(/AppleWebKit.*Mobile.*/) || u.indexOf('Mac OS X') > -1;
+    if (AdrType) {
+        window.location.href = SDANDROID;
+    } else if (iOSType) {
+        window.location.href = SDIOS;
+    }
+}
